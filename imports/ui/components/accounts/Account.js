a7d6f3bf68cbd9a991806410ba9dd16c7ebaad00
gpareek <gpareek@uncc.edu>
2018-04-07 18:56:32 -0400

    Accounts Transaction History UI

--- imports/ui/components/accounts/Account.js
+++ imports/ui/components/accounts/Account.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router"
-import { Button,Jumbotron } from 'react-bootstrap';
+import { Button,Jumbotron,Grid,Row,Col } from 'react-bootstrap';
+import AccountsVerticalTab from "../accounts/AccountsVerticalTab";
 
 export default class Account extends React.Component{
     render(){
         return (
             <Jumbotron>
-                <div className="tabsCustom">
-                    <h1>Account Tab</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-                    <p>
-                    </p>
-                </div>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col>
+                            <AccountsVerticalTab/>
+                        </Col>
+                    </Row>
+                </Grid>
             </Jumbotron>
         );
     }
