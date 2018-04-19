5244997ad00cf0d29410837a45b90060f7b3288c
razad <razad@uncc.edu>
2018-04-18 22:18:09 -0400

    Account Transactions

---
+++ imports/ui/components/accounts/Transaction.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+
+export default class Transaction extends Component{
+
+    render(){
+        return(
+            <li >
+            <span >
+          <strong> {this.props.transaction.cryptototal}</strong>
+
+        </span>
+                <span> {this.props.transaction.cryptocurrency}  ------- $ {this.props.transaction.bankAmount}</span>
+            </li>
+
+        );
+    }
+}
